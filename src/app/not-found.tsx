cd0b80dceef69cb78bac43086e0d3a6d64a759c8
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-11-30 23:43:57 +0530

    removed eslint error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <section className='not-found-container'>
       <div className='not-found-wrapper'>
         <h1 style={{ fontSize: '5rem', color: '#1F86D03D' }}>404</h1>
-        <h2 style={{ color: '#1F86D0' }}>Oops! Page Not Found</h2>
+        <h2 style={{ color: '#1F86D0' }}>Oops Page Not Found</h2>
         <p>
           It seems we can't find the page you're looking for, Let's get you back
           on track!
